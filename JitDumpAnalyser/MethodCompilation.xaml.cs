c0f631aa29d03d0d03b4b2279bf0de20d910c4c0
Andrii Kurdiumov <kant2002@gmail.com>
2022-06-02 20:43:44 +0600

    Add fixed width font and ability to copy phase to clipboard

--- JitDumpAnalyser/MethodCompilation.xaml.cs
+++ JitDumpAnalyser/MethodCompilation.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using JitDumpAnalyser.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Windows.ApplicationModel.DataTransfer;
 
 namespace JitDumpAnalyser;
 
@@ -12,5 +13,13 @@ public sealed partial class MethodCompilation : UserControl
     public MethodCompilation()
     {
         this.InitializeComponent();
+    }
+
+    private void KeyboardAccelerator_Invoked(Microsoft.UI.Xaml.Input.KeyboardAccelerator sender, Microsoft.UI.Xaml.Input.KeyboardAcceleratorInvokedEventArgs args)
+    {
+        DataPackage dataPackage = new DataPackage();
+        dataPackage.RequestedOperation = DataPackageOperation.Copy;
+        dataPackage.SetText(((TextBlock)args.Element).Text);
+        Clipboard.SetContent(dataPackage);
     }
 }
