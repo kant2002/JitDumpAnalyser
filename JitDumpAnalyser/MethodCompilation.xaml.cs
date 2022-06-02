597756534a9ad432df82d1b74d7b86cb051d0a3e
Andrii Kurdiumov <kant2002@gmail.com>
2022-06-02 19:41:18 +0600

    Extract component for method compilation

--- JitDumpAnalyser/MethodCompilation.xaml.cs
+++ JitDumpAnalyser/MethodCompilation.xaml.cs
@@ -1,17 +1,16 @@
 ﻿using JitDumpAnalyser.ViewModels;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
 namespace JitDumpAnalyser;
 
 /// <summary>
 /// An empty window that can be used on its own or navigated to within a Frame.
 /// </summary>
-public sealed partial class MainWindow : Window
+public sealed partial class MethodCompilation : UserControl
 {
-    public MainWindow()
+    public MethodCompilation()
     {
         this.InitializeComponent();
-        parent.DataContext = new MainViewModel(new WinUIFileSelector(this));
-        this.Title = "JIT Dump Analyzer";
     }
 }
