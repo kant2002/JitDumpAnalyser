2d8981a5a7c97450b3075a8be23e6de9b6004cab
Andrii Kurdiumov <kant2002@gmail.com>
2022-06-03 17:55:20 +0600

    Add simple console app for validation of parsing

---
+++ JitDumpAnalyser.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using JitDumpAnalyser.Core;
+
+var content = File.ReadAllText(args[0]);
+
+var parser = new DumpParser();
+var parseResult = parser.Parse(content);
+
+using var streamWriter = new StreamWriter(args[1]);
+foreach (var method in parseResult.ParsedMethods)
+{
+    // Okay
+    // await streamWriter.WriteLineAsync(method.Content);
+    await SerializeMethodCompilationAsync(streamWriter, method);
+}
+
+await streamWriter.FlushAsync();
+streamWriter.Close();
+
+static async Task SerializeMethodCompilationAsync(StreamWriter streamWriter, MethodCompilationResult method)
+{
+    // await streamWriter.WriteLineAsync($"****** START compiling {method.MethodName} (MethodHash={method.MethodHash:X2})");
+    foreach (var phase in method.Phases)
+    {
+        if (phase.PreInfo != null)
+        {
+            await streamWriter.WriteAsync(phase.PreInfo);
+        }
+
+        await streamWriter.WriteAsync(phase.Content);
+        if (phase.PostInfo != null)
+        {
+            await streamWriter.WriteAsync(phase.PostInfo);
+        }
+
+        await streamWriter.WriteLineAsync();
+    }
+}
